eae1414f9d3298135ce6c42cf1f507021cdc0c7d
v167975 <wolfgang.rassek@generali.de>
2016-06-08 16:13:34 +0200

    no power operation when arg2 < 0

--- org.eclipse.example.calc/src/org/eclipse/example/calc/internal/operations/Power.java
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/operations/Power.java
@@ -12,8 +12,14 @@ public class Power implements BinaryOperation {
 
 	@Override
 	public float perform(float arg1, float arg2) {
-		// TODO Auto-generated method stub
-		return (float) Math.pow(arg1, arg2);
+		float f = 0;
+		if (arg2 < 0) {
+			System.out.println("arg2 kleiner 0, arg2=" + arg2);
+		}
+		else { 
+			f = (float) Math.pow(arg1, arg2);
+		}
+		return f;
 	}
 
 }

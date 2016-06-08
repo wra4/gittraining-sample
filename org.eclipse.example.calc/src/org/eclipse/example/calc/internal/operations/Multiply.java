f5a3264d337bb9f7a855cf80cf7c1fca454aa6e3
v167975 <wolfgang.rassek@generali.de>
2016-06-08 14:25:02 +0200

    new function Multiply

---
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/operations/Multiply.java
@@ -0,0 +1,19 @@
+package org.eclipse.example.calc.internal.operations;
+
+import org.eclipse.example.calc.BinaryOperation;
+
+public class Multiply implements BinaryOperation {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public float perform(float arg1, float arg2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}

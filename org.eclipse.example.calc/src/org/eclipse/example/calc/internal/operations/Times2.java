87cabfbeaf3b6e95feda38a29927514f13c4cbb2
v167975 <wolfgang.rassek@generali.de>
2016-06-08 13:58:05 +0200

    adds Calc-Operation Times2

---
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/operations/Times2.java
@@ -0,0 +1,20 @@
+package org.eclipse.example.calc.internal.operations;
+
+import org.eclipse.example.calc.BinaryOperation;
+
+public class Times2 implements BinaryOperation {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "*2";
+	}
+
+	@Override
+	public float perform(float arg1, float arg2) {
+		// TODO Auto-generated method stub
+		System.out.println("ignoring arg2=" + arg2);
+		return arg1 * 2;
+	}
+
+}

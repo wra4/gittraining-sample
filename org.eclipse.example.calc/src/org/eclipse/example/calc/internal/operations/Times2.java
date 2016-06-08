3d45e3fee95193fbc82a77c032be2e78a0319e91
v167975 <wolfgang.rassek@generali.de>
2016-06-08 14:44:49 +0200

    TODO clarified

--- org.eclipse.example.calc/src/org/eclipse/example/calc/internal/operations/Times2.java
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/operations/Times2.java
@@ -12,7 +12,7 @@ public class Times2 implements BinaryOperation {
 
 	@Override
 	public float perform(float arg1, float arg2) {
-		// TODO Auto-generated method stub
+		// TODO: what shall we do with arg2?
 		System.out.println("ignoring arg2=" + arg2);
 		return arg1 * 2;
 	}
